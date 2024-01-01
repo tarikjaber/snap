952a9ef2c83e80f8966e76d2589cb0fdd14420bb
tarikjaber <tariknjaber@gmail.com>
2024-01-01 02:32:15 -0500

    Add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+## Snap
+This script attempts to get the iron man snap at exactly midnight. 
+
+Make sure to have system time synchronized for best results.
--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import subprocess
 import time
 
 # The time of the snap in the video (hours, minutes, seconds)
-snap_time = datetime.timedelta(minutes=1, seconds=11, milliseconds=800)  # 1 minute and 11 seconds before the snap
+snap_time = datetime.timedelta(minutes=1, seconds=11, milliseconds=910)  # 1 minute and 11 seconds before the snap
 
 # Calculate when to start the video
 now = datetime.datetime.now()
-midnight = datetime.datetime.combine(now.date() + datetime.timedelta(days=1), datetime.time(0, 0, 0))  # Midnight of the current day
-midnight = datetime.datetime.combine(now.date(), datetime.time(21, 34, 0))  # Midnight of the current day
+# midnight = datetime.datetime.combine(now.date() + datetime.timedelta(days=1), datetime.time(0, 0, 0))  # Midnight of the current day
+midnight = datetime.datetime.combine(now.date(), datetime.time(2, 30, 0))  # Midnight of the current day
 start_time = midnight - snap_time
 
 # Check if the current time is past the start time
