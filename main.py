1c51d9b8887ab5eaa145429bbb8d62941bc3aa7b
Yosef Jaber <yusefj816@gmail.com>
2024-12-13 23:27:29 -0500

    Improvements

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
 import datetime
 import subprocess
 import time
+import sys
 
-# The time of the snap in the video (hours, minutes, seconds)
-snap_time = datetime.timedelta(minutes=1, seconds=11, milliseconds=910)  # 1 minute and 11 seconds before the snap
+snap_time_video = datetime.timedelta(minutes=1, seconds=11, milliseconds=910)
+snap_time_video = datetime.timedelta(minutes=1, seconds=12, milliseconds=100)
 
-# Calculate when to start the video
 now = datetime.datetime.now()
-midnight = datetime.datetime.combine(now.date() + datetime.timedelta(days=1), datetime.time(0, 0, 0))  # Midnight of the current day
-# midnight = datetime.datetime.combine(now.date(), datetime.time(2, 30, 0))  # Midnight of the current day
-start_time = midnight - snap_time
 
-# Check if the current time is past the start time
+val = sys.argv[1]
+assert(len(val) == 3 or len(val) == 4)
+
+val = val.zfill(4)
+
+day_shift = int(val) // 2400
+hour = int(val[0:2])
+minute = int(val[2:4])
+
+snap_time = datetime.time(hour, minute)
+snap_day = now.date() + datetime.timedelta(days=day_shift)
+
+time_to_snap = datetime.datetime.combine(snap_day, snap_time)
+start_time = time_to_snap - snap_time_video
+
 if now >= start_time:
-    # Calculate how far into the video we should be
+    print("Video start before now")
+
     elapsed_time_since_start = now - start_time
+
     seconds_into_video = elapsed_time_since_start.total_seconds()
 
-    # Start the video at the calculated position
-    subprocess.run(["mpv", f"--start={seconds_into_video}", "snap.mkv"])
+    _ = subprocess.run(["mpv", f"--start={seconds_into_video}", "snap.mkv"])
 else:
-    # Wait until it's time to start the video
-    time.sleep((start_time - now).total_seconds())
+    print("Video start after now")
 
-    # Start the video from the beginning
-    subprocess.run(["mpv", "snap.mkv"])
+    seconds_to_sleep = (start_time - now).total_seconds()
+    print(f"Seconds to sleep = {seconds_to_sleep}")
+
+    time.sleep(seconds_to_sleep)
+    _ = subprocess.run(["mpv", "snap.mkv"])
