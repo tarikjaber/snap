c93ea52bf51600dca3c35a637b2c4ce7bd8e7862
Yosef Jaber <yusefj816@gmail.com>
2025-01-01 01:27:16 -0500

    updates

--- main.py
+++ main.py
@@ -3,19 +3,25 @@ import subprocess
 import time
 import sys
 
-snap_time_video = datetime.timedelta(minutes=1, seconds=11, milliseconds=910)
+start_execution = time.time()
+
 snap_time_video = datetime.timedelta(minutes=1, seconds=12, milliseconds=100)
+snap_time_video = datetime.timedelta(minutes=1, seconds=11, milliseconds=540)
+snap_time_video = datetime.timedelta(minutes=1, seconds=11, milliseconds=974)
 
 now = datetime.datetime.now()
 
 val = sys.argv[1]
-assert(len(val) == 3 or len(val) == 4)
+assert(len(val) < 5 and val.isnumeric())
 
 val = val.zfill(4)
 
 day_shift = int(val) // 2400
+
 hour = int(val[0:2]) % 24
+assert(0 <= hour < 24)
 minute = int(val[2:4])
+assert(0 <= minute < 60)
 
 snap_time = datetime.time(hour, minute)
 snap_day = now.date() + datetime.timedelta(days=day_shift)
@@ -30,6 +36,8 @@ if now >= start_time:
 
     seconds_into_video = elapsed_time_since_start.total_seconds()
 
+    end_execution = time.time()
+    print(end_execution - start_execution)
     _ = subprocess.run(["mpv", f"--start={seconds_into_video}", "snap.mkv"])
 else:
     print("Video start after now")
